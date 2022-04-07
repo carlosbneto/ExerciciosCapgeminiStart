57a9e8a5cd7d8ebd440fc837c6c8c35e7be89fd2
Anselmo Carlos <anselmo.ocarlos@gmail.com>
2022-04-06 22:13:26 -0300

    First commit

---
+++ src/com/startcapgemini/Exercicio1.java
@@ -0,0 +1,28 @@
+package com.startcapgemini;
+
+import java.util.Scanner;
+
+/*Faça um algoritmo que receba dois números e
+exiba o resultado da sua soma;
+ */
+
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        float numero1;
+        float numero2;
+        float resultado;
+
+        System.out.println("Digite o primeiro número: ");
+        numero1 = leitor.nextFloat();
+
+        System.out.println("Digite o segundo número: ");
+        numero2 = leitor.nextFloat();
+
+        resultado = numero1 + numero2;
+        System.out.println("A soma dos números é: " + resultado);
+
+    }
+}
